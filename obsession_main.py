1953d1b9db267bdd6b1e67ff3816cd279400b24a
saundo <cs@qz.com>
2017-04-11 17:38:39 -0400

    Updated obsession main to incorporate new macro trend edits

--- obsession_main.py
+++ obsession_main.py
@@ -14,26 +14,27 @@ import engaged_time_exec
 t1 = {'start':'2017-03-01', 'end':'2017-03-03'}
 t2 = {'start':'2017-03-03', 'end':'2017-03-05'}
 
-def macro_trend(t1, t2):
+def macro_trend(t1, t2, dump_dir, API_interval=24):
     """returns high level trends, t2 / t1
     t1 and t2 MUST be dictionaries of the form:
     t2 = {'start':'2017-03-03', 'end':'2017-03-05'}
     
     returns 5 objects:
-    t1pv - keen API return: page views, articles and obsessions, t1
-    t2pv - keen API return: page views, articles and obsessions, t2
-    t1time - keen API return: total time, articles and obsessions, t1 
-    t2time - keen API return: total time, articles and obsessions, t2
+    pv1 - keen API return: page views, articles and obsessions, t1
+    pv2 - keen API return: page views, articles and obsessions, t2
+    t1 - keen API return: total time, articles and obsessions, t1 
+    t2 - keen API return: total time, articles and obsessions, t2
     tc - sorted DataFrame, merging pvs and time, calculating difference
     """
-    t1pv, t2pv, t1time, t2time, tc = macro_trend_exec.main(t1, t2)
+    pv1, pv2, t1, t2, tc = macro_trend_exec.main(t1, t2, dump_dir, 
+                                                 API_interval=API_interval)
 
     print('obsession pv change', tc.iloc[1:]['pv t2'].sum() / 
           tc.iloc[1:]['pv t1'].sum())
     print('obsession time change', tc.iloc[1:]['time t2'].sum() / 
       tc.iloc[1:]['time t1'].sum())
     
-    return t1pv, t2pv, t1time, t2time, tc
+    return pv1, pv2, t1, t2, tc
 
 def hours_per_article(pv_data, time_data, num=500):
     """num is the minimum number of page views to include in this analysis
