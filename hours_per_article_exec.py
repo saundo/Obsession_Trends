40811828ae7d166423f1dfdaa49e3e5d7f8336e7
saundo <cs@qz.com>
2017-04-11 17:20:32 -0400

    Major changes to hours_per_article_exec
    Lot’s of changes following the HSBC analysts - mainly that this
    function is now built to handle df inputs

--- hours_per_article_exec.py
+++ hours_per_article_exec.py
@@ -7,6 +7,7 @@ Formula - for a given obsession sum of total time spent for all articles,
 divided by number of articles that comprise of that obsession
 """
 
+import numpy as np
 import pandas as pd
 
 def scrub_blank(val):    
@@ -16,7 +17,45 @@ def scrub_blank(val):
         return "## NO OBSESSION ##"
     else:
         return val
-def main(pv_data, time_data, num=500):
+    
+def unique_obsessions(dfpv, dftime, num = 500):
+    """
+    """
+    dfpv = dfpv.groupby('article.id').sum().reset_index()
+    dfpv= dfpv[dfpv['result'] >= num]
+    dftime = dftime.groupby('article.id').sum().reset_index()
+    df = dfpv.merge(dftime, on='article.id')
+    
+    #remove articles that don't make the cut; IN says that the articles are in the article_list
+    article_list = set(df['article.id'])
+    dfpv['in'] = dfpv['article.id'].apply(lambda x: x in article_list)
+    
+    #do a first cut of removing duplicates
+    dft = dfpv[dfpv['in'] == True]
+    dft = dft[['article.id', 'article.obsessions']].drop_duplicates()
+    
+    #find the remaining duplicates
+    dftt = dft.groupby('article.id').count().reset_index()
+    remaining_dupes = set(dftt[dftt['article.obsessions'] > 1]['article.id'])
+    
+    dft['dupe'] = dft['article.id'].apply(lambda x: x in remaining_dupes)
+    dft_unique = dft[dft['dupe'] != True]
+    dft_dupe = dft[dft['dupe'] == True].copy()
+    
+    obsess_storage = {}
+    tracker = len(set(dft_dupe['article.id']))
+    for i, article in enumerate(set(dft_dupe['article.id'])):
+        x1 = dfpv[dfpv['article.id'] == article]
+        obsess_storage.setdefault('article.obsessions', []).append(x1['article.obsessions'].values[0])
+        obsess_storage.setdefault('article.id', []).append(article)
+        if int(i / tracker * 100) % 10 == 0:
+            print(int(i / tracker * 100), '% ', end='')        
+        
+    df_FINAL = dft_unique[['article.id', 'article.obsessions']].append(pd.DataFrame(obsess_storage))
+    return df_FINAL
+
+
+def main(dfpv, dftime, num=500):
     """primary function: pv_data and time_data should be from same time 
     interval; 
     num refers to threshold of pageviews; so default num=500 means that only
@@ -24,30 +63,28 @@ def main(pv_data, time_data, num=500):
     returns- sorted DataFrame
     """
     
-    df = pd.DataFrame(pv_data)
-    df1 = df[df['result'] >= num]
+    dfpv = dfpv.groupby('article.id').sum().reset_index()
+    dfpv = dfpv[dfpv['result'] >= num]
+    dftime = dftime.groupby('article.id').sum().reset_index()
+    df = dfpv.merge(dftime, on='article.id')
+    df.columns = ['article.id', 'page views', 'in', 'time']
+    df = df[['article.id', 'page views', 'time']]
     
-    df2 = pd.DataFrame(time_data)
-    df3 = df1.merge(df2, on='article.id')
-    df3 = df3.applymap(scrub_blank)
+    df_unique_obsess = unique_obsessions(dfpv, dftime, num=num)
+    df = df.merge(df_unique_obsess, on='article.id')
+    df = df.sort_values('page views', ascending=False)
     
     storage = {}
-    for article in set(df3['article.id']):
-        dft = df3[df3['article.id'] == article]
-        dft = dft.sort_values('result_y', ascending=False)
-        max_obsess = dft.iloc[0,3]
-        dft = dft[(dft.iloc[:,1] == max_obsess) & (dft.iloc[:,3] == max_obsess)]
-        storage.setdefault('article.id', []).append(article)
-        storage.setdefault('obsession', []).append(max_obsess)
-        storage.setdefault('total time spent', []).append(int(dft.iloc[:,4].values))
-    
-    df = pd.DataFrame(storage)
-    storage = {}    
-    for obsession in set(df['obsession']):
-        dft = df[df['obsession'] == obsession]
-        x1 = (dft['total time spent'].sum() / len(dft)) / 3600
-        storage.setdefault('avg hours per article', []).append(int(x1))
+    for obsession in set(df['article.obsessions']):
+        dft = df[df['article.obsessions'] == obsession]
+        storage.setdefault('total time (h)', []).append(int(dft['time'].sum() / 3600))
+        storage.setdefault('unique articles > 500 pv', []).append(dft['article.id'].nunique())
         storage.setdefault('obsession', []).append(obsession)
     
-    return pd.DataFrame(storage).sort_values('avg hours per article', 
-                       ascending=False)
+    df_obsess_totals = pd.DataFrame(storage).sort_values('total time (h)', ascending=False)
+    df_obsess_totals['avg time per article (m)'] = (df_obsess_totals['total time (h)'] / df_obsess_totals['unique articles > 500 pv']) / 60
+    
+    df_obsess_totals = df_obsess_totals.sort_values('avg time per article (m)', ascending=False)
+    df_obsess_totals.index = np.arange(len(df_obsess_totals))
+    
+    return df_obsess_totals
